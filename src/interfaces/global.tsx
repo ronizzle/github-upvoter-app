9d8f6b2b652b57fc471a5b33cbb0045c6beaa606
Roniel Bernas <roniel44@gmail.com>
2022-05-24 14:45:43 +0800

    Add new props to Repo interface.

--- src/interfaces/global.tsx
+++ src/interfaces/global.tsx
@@ -1,7 +1,20 @@
 export interface IRepo {
   id: string;
   name: string;
+  full_name: string;
   html_url: string;
+  description: string;
+  created_at: string;
+  language: string;
+  license: {
+    name: string;
+    url: string;
+  };
+  owner: {
+    avatar_url: string;
+    html_url: string;
+    login: string;
+  };
 }
 
 export interface RepoProps {
